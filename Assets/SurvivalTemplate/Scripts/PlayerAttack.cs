5635445e4cdc34fd4644e14b0b02eb5db51b541b
pesetskyps <pesetskyps@gmail.com>
2015-01-18 21:03:12 +0300

    second

---
+++ Assets/SurvivalTemplate/Scripts/PlayerAttack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour
+{
+		public Transform AttackPoint;
+
+		void Attack ()
+		{
+			var hits = Physics.OverlapSphere (AttackPoint.position, 0.5f);
+			foreach (var hit in hits) {
+				var hitables = hit.GetComponents (typeof(IHittable));
+				if(hitables == null)
+					return;
+				foreach (IHittable hittable in hitables) {
+					hittable.Hit();
+				}
+			}
+		}
+}
